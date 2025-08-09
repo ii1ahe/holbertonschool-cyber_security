84eaa3925daad53a98f91f24145165b3cded18ee
ii1ahe <ilaheqedirli16@gmail.com>
2025-08-09 21:09:45 +0200

    Update 4-root_process.sh

--- cybersecurity_basics/0x00_introduction_cybersecurity/4-root_process.sh
+++ cybersecurity_basics/0x00_introduction_cybersecurity/4-root_process.sh
@@ -1,14 +1,2 @@
 #!/bin/bash
-# Script to monitor all processes started by a specified user
-# Usage: ./4-root_process.sh <username>
-
-if [ -z "$1" ]; then
-    echo "Usage: $0 <username>"
-    exit 1
-fi
-
-USER_TO_MONITOR="$1"
-
-# List processes for the user, excluding those with VSZ and RSS of 0
-ps -u "$USER_TO_MONITOR" -o user,pid,%cpu,%mem,vsz,rss,tty,stat,start,time,cmd \
-    | grep -v " 0  0$"
+ps -u "$1" -o user,pid,%cpu,%mem,vsz,rss,tty,stat,start,time,cmd --no-headers | grep -vE ' [0]*0 +[0]*0 '
