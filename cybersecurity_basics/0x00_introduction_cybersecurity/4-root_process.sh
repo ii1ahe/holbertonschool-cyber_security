129b1d0a36a0987621011c195c63b1d4f971a398
kali <kali@kali.kali>
2025-08-09 15:01:12 -0400

    4

--- cybersecurity_basics/0x00_introduction_cybersecurity/4-root_process.sh
+++ cybersecurity_basics/0x00_introduction_cybersecurity/4-root_process.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-ps aux | grep -E "^$1[[:space:]]" | grep -Ev "[[:space:]]0[[:space:]]+0[[:space:]]"
+# Script to monitor all processes started by a specified user
+# Usage: ./4-root_process.sh <username>
+
+if [ -z "$1" ]; then
+    echo "Usage: $0 <username>"
+    exit 1
+fi
+
+USER_TO_MONITOR="$1"
+
+# List processes for the user, excluding those with VSZ and RSS of 0
+ps -u "$USER_TO_MONITOR" -o user,pid,%cpu,%mem,vsz,rss,tty,stat,start,time,cmd \
+    | grep -v " 0  0$"
