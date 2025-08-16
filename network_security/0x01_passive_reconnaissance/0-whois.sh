6cd2786e593f883c0a99045aa39fdfc06d074308
ii1ahe <ilaheqedirli16@gmail.com>
2025-08-16 23:57:33 +0400

    Update 0-whois.sh

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois "$1" | awk -F':' '/^(Registrant|Admin|Tech) /{k=$1; sub(/[[:space:]]+$/,"",k); v=$0; sub(/^[^:]*: */,"",v); print k "," v}' > "$1.csv"
+whois $1 | awk '/^(Registrant|Admin|Tech) (Name|Organization|Street|City|State\/Province|Postal Code|Country|Phone|Phone Ext:|Fax|Fax Ext:|Email)/{l=$0;i=index(l,":");key=substr(l,1,i-1);val=substr(l,i+1);sub(/^[ \t]+/,"",val);sub(/[ \t]+$/,"",val);print key","val}' > $1.csv
