01f5e714c413b857432ee7ca0f10d67d69449a43
ii1ahe <ilaheqedirli16@gmail.com>
2025-09-14 02:24:08 -0400

    Update 1-host_header_injection.sh

--- web_application_security/0x00_web_fundamentals/1-host_header_injection.sh
+++ web_application_security/0x00_web_fundamentals/1-host_header_injection.sh
@@ -1,11 +1,2 @@
 #!/bin/bash
-# Usage: ./1-host_header_injection.sh <NEW_HOST> <TARGET_URL> <FORM_DATA>
-
-new_host="$1"   # e.g., host_changed
-target_url="$2" # e.g., https://httpbin.org/post
-form_data="$3"  # e.g., input=hbtn_test@hbtn.hbtn
-
-# Send the POST with a forged Host header. Output the response body to stdout.
-curl -s -X POST "$target_url" \
-  -H "Host: $new_host" \
-  --data "$form_data"
+curl -s -X POST -H "Host: $1" -d "$3" "$2"
