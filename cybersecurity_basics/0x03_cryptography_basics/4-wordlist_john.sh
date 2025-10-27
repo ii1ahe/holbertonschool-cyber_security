275ffe3cc7acb39150108380e4eb84a0b605545c
ii1ahe <ilaheqedirli16@gmail.com>
2025-10-27 03:41:22 +0000

    4

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-john --wordlist=/usr/share/wordlists/rockyou.txt "$1"
-john --show "$1" | awk -F: '{print $2}' > 4-password.txt
+bash -c $'john --wordlist=/usr/share/wordlists/rockyou.txt "$1"\njohn --show "$1" | awk -F: "{print $2}" > 4-password.txt' _ "$1"
 
