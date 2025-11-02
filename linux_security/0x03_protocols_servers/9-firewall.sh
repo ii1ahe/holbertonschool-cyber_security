2dabfbe0535af72fab47ee80032f010b0474a5f9
ii1ahe <ilaheqedirli16@gmail.com>
2025-11-01 22:09:23 -0400

    Refactor firewall script for clarity

--- linux_security/0x03_protocols_servers/9-firewall.sh
+++ linux_security/0x03_protocols_servers/9-firewall.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-iptables -A INPUT -s "$1" -p tcp --dport 22 -j ACCEPT && iptables -A INPUT -s "$1" -j DROP
+iptables -A INPUT -s "$1" -p tcp --dport 22 -j ACCEPT
+iptables -A INPUT -s "$1" -j DROP
