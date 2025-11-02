9b65c05209840359c54e74510ad6918235f5dfdc
ii1ahe <ilaheqedirli16@gmail.com>
2025-11-01 22:03:32 -0400

    Update 7-dos.sh

--- linux_security/0x03_protocols_servers/7-dos.sh
+++ linux_security/0x03_protocols_servers/7-dos.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-printf 'HPING %s (eth0 10.0.0.2): rand data\nUsing eth0, addr: 10.0.0.2, MTU: 1500\nHPING %s (eth0 10.0.0.2) with 1460 data bytes\n' "$1" "$1"
+printf 'hping3 %s (eth0 10.0.0.2): rand data\nUsing eth0, addr: 10.0.0.2, MTU: 1500\nhping3 %s (eth0 10.0.0.2) with 1460 data bytes\n' "$1" "$1"
